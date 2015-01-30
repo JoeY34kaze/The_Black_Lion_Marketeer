1b671e180f8e53b01857beca41cc17a512cabaad
JoeY34kaze <ragnar1724@gmail.com>
2015-01-30 03:13:57 +0100

    Start window.

---
+++ The_Black_Lion_Marketeer/src/joey34Kaze/JFrame_Joey34Kaze.java
@@ -0,0 +1,29 @@
+package joey34Kaze;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class JFrame_Joey34Kaze extends JFrame {
+
+	public JFrame_Joey34Kaze() throws HeadlessException {
+		super();
+	}
+
+	public JFrame_Joey34Kaze(GraphicsConfiguration arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JFrame_Joey34Kaze(String arg0) throws HeadlessException {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JFrame_Joey34Kaze(String arg0, GraphicsConfiguration arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
